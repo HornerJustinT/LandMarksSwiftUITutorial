18199db82e6ced467fd9ec149c24d44e639e05c9
Justin <Justin.Horner@revature.net>
2021-06-17 12:05:36 -0500

    Initial Commit

---
+++ LandMarksSwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMarksSwiftUITutorial
+//
+//  Created by Justin Horner on 6/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LandMarksSwiftUITutorial/LandMarksSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksSwiftUITutorialApp.swift
+//  LandMarksSwiftUITutorial
+//
+//  Created by Justin Horner on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
